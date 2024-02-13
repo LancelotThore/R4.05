8012d2d0ff43c291ddb694aecdaca2bc46ecc8e6
LancelotThore <144225580+LancelotThore@users.noreply.github.com>
2024-02-13 08:31:00 +0100

    exercice 2

--- TP3/exo2/exo.js
+++ TP3/exo2/exo.js
@@ -12,15 +12,15 @@ container.appendChild(stats.dom);
 // Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.CubeTextureLoader()
-	.setPath( 'assets/' )
-	.load( [
-                'posx.jpg',
-				'negx.jpg',
-                'posy.jpg',
-                'negy.jpg',
-                'posz.jpg',
-                'negz.jpg'
-			] );
+    .setPath('assets/')
+    .load([
+        'posx.jpg',
+        'negx.jpg',
+        'posy.jpg',
+        'negy.jpg',
+        'posz.jpg',
+        'negz.jpg'
+    ]);
 
 // an array of objects whose rotation to update
 const objects = [];
@@ -28,49 +28,43 @@ const objects = [];
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
-function onPointerMove( event ) {
+function onPointerMove(event) {
 
-	// calculate pointer position in normalized device coordinates
-	// (-1 to +1) for both components
+    // calculate pointer position in normalized device coordinates
+    // (-1 to +1) for both components
 
-	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
-    console.log(pointer)
 }
 
 
-window.addEventListener( 'pointermove', onPointerMove );
+window.addEventListener('pointermove', onPointerMove);
 
 // plane
-const planeGeometry = new THREE.PlaneGeometry( 25, 25, 32, 32 );
-const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xFFBBAC, side: THREE.DoubleSide } )
-const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+const planeGeometry = new THREE.PlaneGeometry(25, 25, 32, 32);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xFFBBAC, side: THREE.DoubleSide })
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true;
 plane.rotation.set(Math.PI * -0.5, 0, 0);
-scene.add( plane );
+scene.add(plane);
 
 const loader = new GLTFLoader();
 
 let model;
 
-loader.load( './obj/Rocketship.glb', function ( glb ) {
+loader.load('./obj/Rocketship.glb', function (glb) {
     model = glb.scene;
-	scene.add( model );
+    scene.add(model);
     model.scale.set(0.5, 0.5, 0.5);
     model.rotateY(Math.PI / 2);
 
-    model.traverse(function(node) {
-        if(node.isMesh) {
+    model.traverse(function (node) {
+        if (node.isMesh) {
             node.castShadow = true;
         }
     })
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
+});
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
@@ -80,7 +74,6 @@ camera.lookAt(0, 0, 0);
 
 // const grille = new THREE.GridHelper(25, 25);
 // scene.add(grille);
-
 
 
 window.addEventListener('resize', () => {
@@ -107,8 +100,8 @@ light.shadow.camera.right = -100;
 light.shadow.camera.top = 100;
 light.shadow.camera.bottom = -100;
 
-const shadowHelper = new THREE.CameraHelper( light.shadow.camera );
-scene.add( shadowHelper );
+const shadowHelper = new THREE.CameraHelper(light.shadow.camera);
+scene.add(shadowHelper);
 
 // Renderer
 const canvas = document.querySelector(".webgl");
@@ -122,20 +115,67 @@ renderer.render(scene, camera);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-let rot = 0;
 
-const loop = () => {
-    rot += 0.1;
-    if(model) {
-        if(model.position.y > 20) {
-            rot = 0;
-            model.position.set(0, 0, 0)
+let speed = 0;
+
+const animation = () => {
+    
+    if (scene.children[4]!=undefined) {
+        if (speed<10) {
+            speed+=0.1;
+            scene.children[4].position.y = speed;    
+            
+            window.requestAnimationFrame(animation);
         }
         else {
-            model.position.set(0, rot, 0);
+            scene.children[4].position.y =0;
         }
     }
-    
+    renderer.render(scene, camera);
+}
+
+function onPointerClick( event ) {
+
+	// calculate pointer position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    window.requestAnimationFrame(render);
+
+}
+
+function render() {
+	// update the picking ray with the camera and pointer position
+	raycaster.setFromCamera( pointer, camera );
+
+	// calculate objects intersecting the picking ray
+	const intersects = raycaster.intersectObjects( scene.children );
+
+	for ( let i = 0; i < intersects.length; i ++ ) {
+        
+        // intersects[ i ].object.material.color.set( 0xff0000 );
+        if (intersects[i].object.castShadow==true) {
+            scene.children[4].position.y = 0;
+            speed = 0;
+            animation();       
+        }
+	}
+}
+
+const loop = () => {
+    speed += 0.1;
+    // if (model) {
+    //     if (model.position.y > 20) {
+    //         speed = 0;
+    //         model.position.set(0, 0, 0)
+    //     }
+    //     else {
+    //         model.position.set(0, speed, 0);
+    //     }
+    // }
+
     controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(loop);
@@ -143,3 +183,5 @@ const loop = () => {
 }
 
 loop();
+
+window.addEventListener( 'pointerdown', onPointerClick );
